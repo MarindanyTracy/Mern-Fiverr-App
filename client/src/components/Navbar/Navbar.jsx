d63a9ae178d2fcb97923bc798fa4d1931d2e829e
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-20 09:24:23 +0300

    React menu with useState

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import "./Navbar.scss";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false)
@@ -14,7 +15,13 @@ const Navbar = () => {
     return () => {
       window.removeEventListener("scroll", isActive);
     }
-  },[])
+  },[]);
+
+  const currentUser = {
+    id: 1,
+    username: "John Doe",
+    isSeller: true
+  }
 
   return (
     <div className={active ? "navbar active" : "navbar"}>
@@ -30,8 +37,27 @@ const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign In</span>
-          <span>Become a Seller</span>
-          <button className="button">Join</button>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button className="button">Join</button>}
+          {currentUser && (
+            <div className="user" onClick={()=> setOpen(!open)}>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9aniZE01uX-uzkRSscxQ4DYTwSUzC7ZKmDtExIg7tKBIH-rCO7-QhHz-kaYiHR9Vpgs4&usqp=CAU" alt="" />
+              <span>{currentUser?.username}</span>
+             {open && <div className="options">
+                {
+                  currentUser?.isSeller && (
+                    <>
+                    <span>Gigs</span>
+                    <span>Add New Gig</span>
+                    </>
+                  )
+                }
+                <span>Orders</span>
+                <span>Messages</span>
+                <span>Logout</span>
+              </div>}
+            </div>
+          )}
         </div>
       </div>
 
