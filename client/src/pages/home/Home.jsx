b82682451c44f708b1fd440f9a1fc2c3203310c8
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-22 12:26:39 +0300

    React Product Slider

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,8 +3,9 @@ import './home.scss';
 import Featured from '../../components/featured/Featured';
 import TrustedBy from '../../components/trustedBy/TrustedBy';
 import Slide from '../../components/Slide/Slide';
-import { cards } from "../../data";
+import { cards, projects } from "../../data";
 import CatCard from '../../components/catCard/CatCard';
+import ProjectCard from '../../components/projectCard/ProjectCard';
 
 const Home = () => {
   return (
@@ -83,6 +84,12 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map(card => (
+          <ProjectCard key={card.id} item={card} />
+        ))}
+      </Slide>
     </div>
   )
 }
