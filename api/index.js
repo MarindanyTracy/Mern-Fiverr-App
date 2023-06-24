720539f353704c5e56d6ef59736949f74510148e
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-24 20:36:54 +0300

    Setting up backend and connecting to mongodb

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB!");
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+  }
+};
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at ${process.env.PORT}`);
+  connect();
+});
