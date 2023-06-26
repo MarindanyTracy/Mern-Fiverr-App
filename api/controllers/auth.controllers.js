8915a9790fb81823b560d9cb40d8e897ccea5554
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-26 12:11:09 +0300

    Error handling

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from 'jsonwebtoken';
+import createError from "../utils/createError.js";
 
 export const register = async (req, res, next) => {
   try {
@@ -13,7 +14,7 @@ export const register = async (req, res, next) => {
     await newUser.save();
     res.status(201).send("User has been created");
   } catch (err) {
-    res.status(500).send("Something went wrong!");
+   next(err);
   }
 };
 
@@ -21,11 +22,11 @@ export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
     if (!user) {
-      return res.status(404).send("User not found");
+      return next(createError(404, "User not found"));
     }
 
     const isCorrect = bcrypt.compare(req.body.password, user.password);
-    if (!isCorrect) return res.status(400).send("Wrong password or usernmae");
+    if (!isCorrect) return next(createError(400, "Wrong password or username"));
 
     const token = jwt.sign({
       id: user._id,
@@ -37,12 +38,16 @@ export const login = async (req, res, next) => {
       httpOnly: true,
     }).status(200).send(info)
   } catch (err) {
-    res.status(500).send("Something went wrong!");
+    next(err);
   }
 };
 
 export const logout = async (req, res, next) => {
   try {
+    res.clearCookie("accessToken", {
+      sameSite: "none",
+      secure: true,
+    }).status(200).send("User has been logged out!")
   } catch (err) {
     res.status(500).send("Something went wrong!");
   }
