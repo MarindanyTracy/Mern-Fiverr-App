3e17ee9e611206873592fc25ebc0b46bd8c3817e
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-26 09:14:42 +0300

    Registering  routes and controllers

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import mongoose, { mongo } from "mongoose";
 import dotenv from "dotenv";
+import userRoute from './routes/user.route.js';
+import reviewRoute from './routes/review.route.js';
+import orderRoute from './routes/order.route.js';
+import messageRoute from './routes/message.route.js';
+import gigRoute from './routes/gig.route.js';
+import conversationRoute from './routes/conversation.route.js';
 
 const app = express();
 dotenv.config();
@@ -14,6 +20,14 @@ const connect = async () => {
     console.log(`Error: ${err.message}`);
   }
 };
+
+app.use("/api/users", userRoute);
+app.use('/api/gigs', gigRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/conversations', conversationRoute);
+app.use('/api/messages', messageRoute);
+app.use('/api/reviews', reviewRoute);
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening at ${process.env.PORT}`);
