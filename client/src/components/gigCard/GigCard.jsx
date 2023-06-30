82f6819c5a745338ea86253ffd166da54ff0b9b8
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-30 13:37:54 +0300

    React Querrying our gig page

--- client/src/components/gigCard/GigCard.jsx
+++ client/src/components/gigCard/GigCard.jsx
@@ -1,21 +1,36 @@
 import React from "react";
 import "./gigCard.scss";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
 
 const GigCard = ({ item }) => {
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["${item.userId"],
+    queryFn: () =>
+      newRequest.get(`/users/${item.userId}`).then((res) => {
+        return res.data;
+      }),
+  });
   return (
-    <Link to="/gig/123" className="link">
+    <Link to={`/gig/${item._id}`} className="link">
       <div className="gigCard">
-        <img src={item.img} alt="" />
+        <img src={item.cover} alt="" />
         <div className="info">
-          <div className="user">
-            <img src={item.pp} alt="" />
-            <span>{item.username}</span>
-          </div>
+          {isLoading ? (
+            "loading"
+          ) : error ? (
+            "Something went wrong"
+          ) : (
+            <div className="user">
+              <img src={data.img || "/images/noavatar.jpg"} alt="" />
+              <span>{data.username}</span>
+            </div>
+          )}
           <p>{item.desc}</p>
           <div className="star">
             <img src="./images/star.png" alt="" />
-            <span>{item.star}</span>
+            <span>{!isNaN(item.totalStars /item.starNumber) && Math.round(item.totalStars /item.starNumber)}</span>
           </div>
         </div>
         <hr />
@@ -34,5 +49,3 @@ const GigCard = ({ item }) => {
   );
 };
 export default GigCard;
-
-
