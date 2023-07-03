e42a2dd1e1c4000d9e3971b0f0c06ee79faee09d
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-03 09:17:18 +0300

    Finishing up on adding a review

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -39,7 +39,7 @@ const Reviews = ({ gigId }) => {
         : data.map((review) => <Review key={review._id} review={review} />)}
         <div className="add">
           <h3>Add a review</h3>
-          <form action=""onSubmit={handleSubmit}>
+          <form action="" className="addForm" onSubmit={handleSubmit}>
             <input type="text" placeholder="Write your opinion" />
             <select name="" id="">
               <option value={1}>1</option>
