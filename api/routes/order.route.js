ecebb6babe2849dd02cf7b2639c0387f73d745d2
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-03 09:58:05 +0300

    Backend functionality for fetching and creating orders

--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import { verifyToken } from '../middleware/jwt.js';
+import { createOrder,getOrders } from '../controllers/order.controllers.js';
 
 const router = express.Router();
 
-router.get("/", );
+router.post("/:gigId",verifyToken, createOrder);
+router.get("/",verifyToken, getOrders);
 
 export default router;
