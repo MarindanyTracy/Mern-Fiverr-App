a685515c149a51089c60634b66ef93d3aa03ccfb
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-04 10:36:30 +0300

    Adding single message functionality

--- api/routes/message.route.js
+++ api/routes/message.route.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import { verifyToken } from '../middleware/jwt.js';
+import { createMessage, getMessages } from '../controllers/message.controllers.js';
 
 const router = express.Router();
 
-router.get("/", );
+router.post("/", verifyToken, createMessage);
+router.get("/:id", verifyToken, getMessages);
 
 export default router;
--- client/src/pages/messages/Messages.jsx
+++ client/src/pages/messages/Messages.jsx
@@ -1,13 +1,14 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
 import { Link } from "react-router-dom";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
 import "./Messages.scss";
 import moment from "moment";
 
 const Messages = () => {
-  const queryClient = useQueryClient();
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  const queryClient = useQueryClient();
 
   const { isLoading, error, data } = useQuery({
     queryKey: ["conversations"],
@@ -19,29 +20,28 @@ const Messages = () => {
 
   const mutation = useMutation({
     mutationFn: (id) => {
-      return newRequest.put(`/conversations/${id}`)
+      return newRequest.put(`/conversations/${id}`);
     },
     onSuccess: () => {
-      queryClient.invalidateQueries(["conversations"])
-    }
-  })
+      queryClient.invalidateQueries(["conversations"]);
+    },
+  });
 
   const handleRead = (id) => {
     mutation.mutate(id);
-  }
+  };
 
   return (
     <div className="messages">
       {isLoading ? (
-        "Loading"
+        "loading"
       ) : error ? (
-        "error occured"
+        "error"
       ) : (
         <div className="container">
           <div className="title">
             <h1>Messages</h1>
           </div>
-
           <table>
             <tr>
               <th>{currentUser.isSeller ? "Buyer" : "Seller"}</th>
@@ -50,20 +50,28 @@ const Messages = () => {
               <th>Action</th>
             </tr>
             {data.map((c) => (
-              <tr className={((currentUser.isSeller && !c.readBySeller) ||
-                (!currentUser.isSeller && !c.readByBuyer)) && "active"} key={c.id}>
+              <tr
+                className={
+                  ((currentUser.isSeller && !c.readBySeller) ||
+                    (!currentUser.isSeller && !c.readByBuyer)) &&
+                  "active"
+                }
+                key={c.id}
+              >
                 <td>{currentUser.isSeller ? c.buyerId : c.sellerId}</td>
                 <td>
-                  <Link to="/message/123" className="link">
+                  <Link to={`/message/${c.id}`} className="link">
                     {c?.lastMessage?.substring(0, 100)}...
                   </Link>
                 </td>
                 <td>{moment(c.updatedAt).fromNow()}</td>
                 <td>
-                  {(currentUser.isSeller && !c.readBySeller) ||
-                    (!currentUser.isSeller && !c.readByBuyer && (
-                      <button onClick={() => handleRead(c.id)}>Mark as Read</button>
-                    ))}
+                  {((currentUser.isSeller && !c.readBySeller) ||
+                    (!currentUser.isSeller && !c.readByBuyer)) && (
+                    <button onClick={() => handleRead(c.id)}>
+                      Mark as Read
+                    </button>
+                  )}
                 </td>
               </tr>
             ))}
