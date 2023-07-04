50f96696dbccc1ce1bdd50c7e7045c1b185a2f34
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-04 11:05:24 +0300

    Wiring up orders to send messages to seller

--- api/controllers/conversation.controllers.js
+++ api/controllers/conversation.controllers.js
@@ -20,9 +20,10 @@ export const createConversation = async (req, res, next) => {
 
 export const getConversations = async (req, res, next) => {
   try {
-    const conversations = await Conversation.find(req.isSeller ? { sellerId: req.userId } : { buyerId: req.userId });
+    const conversations = await Conversation.find(
+      req.isSeller ? { sellerId: req.userId } : { buyerId: req.userId }
+    ).sort({ updatedAt: -1 });
     res.status(201).send(conversations);
-
   } catch (err) {
     next(err);
   }
@@ -31,6 +32,7 @@ export const getConversations = async (req, res, next) => {
 export const getSingleConversation = async (req, res, next) => {
   try {
     const conversation = await Conversation.findOne({ id: req.params.id });
+    if (!conversation) return next(createError(404, "Not Found!"));
     res.status(200).send(conversation);
   } catch (err) {
     next(err);
@@ -43,7 +45,7 @@ export const updateConversation = async (req, res, next) => {
       { id: req.params.id },
       {
         $set: {
-          ...(req.isSeller ? {readBySeller: true} : {readByBuyer: true})
+          ...(req.isSeller ? { readBySeller: true } : { readByBuyer: true }),
         },
       },
       { new: true }
--- client/src/pages/orders/Orders.jsx
+++ client/src/pages/orders/Orders.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./Orders.scss";
 import { useQuery } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
+import { useNavigate, Link } from 'react-router-dom';
 
 const Orders = () => {
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
+  const navigate = useNavigate();
   const { isLoading, error, data } = useQuery({
     queryKey: ["orders"],
     queryFn: () =>
@@ -13,6 +15,25 @@ const Orders = () => {
         return res.data;
       }),
   });
+
+  const handleContact = async (order) => {
+    const sellerId = order.sellerId;
+    const buyerId = order.buyerId;
+    const id = sellerId + buyerId;
+
+    try {
+      const res = await newRequest.get(`/conversations/single/${id}`);
+      navigate(`/message/${res.data.id}`)
+
+    } catch (err) {
+      if (err.response.status === 404) {
+        const res = await newRequest.post(`/conversations/`, {
+          to: currentUser.isSeller ? buyerId : sellerId,
+        });
+        navigate(`/message/${res.data.id}`)
+      }
+    }
+  };
 
   return (
     <div className="orders">
@@ -33,24 +54,23 @@ const Orders = () => {
               <th>Price</th>
               <th>Contact</th>
             </tr>
-           { data.map(order => (
-
-             <tr key={order._id}>
-              <td>
-                <img
-                  className="image"
-                  src={order.img}
-                  alt=""
+            {data.map((order) => (
+              <tr key={order._id}>
+                <td>
+                  <img className="image" src={order.img} alt="" />
+                </td>
+                <td>{order.title}</td>
+                <td>{order.price}</td>
+                <td>
+                  <img
+                    className="message"
+                    src="./images/message.png"
+                    alt=""
+                    onClick={() => handleContact(order)}
                   />
-              </td>
-              <td>{order.title}</td>
-              <td>{order.price}</td>
-              <td>
-                <img className="message" src="./images/message.png" alt="" />
-              </td>
-            </tr>
-                  )) }
-                  
+                </td>
+              </tr>
+            ))}
           </table>
         </div>
       )}
